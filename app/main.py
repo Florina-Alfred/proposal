c39c2c88718ed8deb2558d79836af506aadf853b
Florina-Alfred <sonofcop@gmail.com>
2023-03-30 18:56:51 +0200

    31

--- app/main.py
+++ app/main.py
@@ -6,12 +6,11 @@ TEXT_1 = "Flroina accepted the proposals from Alfred for the 3rd time"
 TEXT_2 = "Alfred proposes to Florina for an infinite number of times"
 
 @app.route('/')
-def index():
+def home():
     return TEXT_1
 
-
 @app.route('/real')
-def index():
+def outside():
     return TEXT_2
 
 app.run(host='0.0.0.0',port=3000)
