e93dff4c712d4c9f11869f6f06677bf7cd954d98
Florina-Alfred <sonofcop@gmail.com>
2023-03-30 18:26:59 +0200

    22

--- app/main.py
+++ app/main.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Alfred proposes to Florina for the 3rd time"
+    return "Flroina accepted the proposals from Alfred for the 3rd time"
 
 app.run(host='0.0.0.0',port=3000)
