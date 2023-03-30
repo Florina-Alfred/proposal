d6cb5c4360117fce7c4d32724b257fc65fec1458
Florina-Alfred <sonofcop@gmail.com>
2023-03-30 18:51:52 +0200

    30

--- app/main.py
+++ app/main.py
@@ -2,8 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+TEXT_1 = "Flroina accepted the proposals from Alfred for the 3rd time"
+TEXT_2 = "Alfred proposes to Florina for an infinite number of times"
+
 @app.route('/')
 def index():
-    return "Flroina accepted the proposals from Alfred for the 3rd time"
+    return TEXT_1
+
+
+@app.route('/real')
+def index():
+    return TEXT_2
 
 app.run(host='0.0.0.0',port=3000)
