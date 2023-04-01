dc5a4d086a49bdb64df208e1c7b37e02b6cb1d4c
Florina-Alfred <sonofcop@gmail.com>
2023-04-01 12:48:58 +0200

    starlite first push

--- app/main.py
+++ app/main.py
@@ -1,17 +1,11 @@
-from flask import Flask, render_template
+from starlite import Starlite, MediaType, get
+import uvicorn
 
-app = Flask(__name__)
+@get(path="/", media_type=MediaType.TEXT)
+def index() -> str:
+    return "hello Florina"
 
-TEXT_1 = "Flroina accepted the proposals from Alfred for the 3rd time"
-TEXT_2 = "Alfred proposes to Florina for an infinite number of times"
 
-@app.route('/')
-def home():
-   return render_template('index.html', bride="Florina", groom="Alfred")
+app = Starlite(route_handlers=[index])
 
-@app.route('/<name>')
-def outside(name):
-    return name
-
-if __name__ == '__main__':
-   app.run(host='0.0.0.0',port=3000)
+uvicorn.run(app, host='0.0.0.0', port=3000)
