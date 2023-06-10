bb3cf754e11f871ebbe6d514604b3a1f3b87730b
Florina-Alfred <sonofcop@gmail.com>
2023-06-10 12:48:19 +0200

    direct image feed rather than Stream feed

--- app/main.py
+++ app/main.py
@@ -31,6 +31,28 @@ def root():
     _, img_jpeg = cv2.imencode(".jpeg", img)
     return StreamingResponse(io.BytesIO(img_jpeg.tobytes()), media_type="image/jpeg")
 
+@app.get(
+    "/image",
+    responses = {200: {"content": {"image/jpeg": {}}}},
+    response_class=Response
+)
+def get_image():
+    img_name = random.choice(glob.glob("./app/static/image_*"))
+    img = cv2.imread(img_name)
+    img_y, img_x, _ = img.shape
+
+    cv2.putText(
+        img=img,
+        text=TEXT,
+        org=(img_x // 2 - (len(TEXT) // 2) * 18, img_y // 5),
+        fontFace=cv2.FONT_HERSHEY_TRIPLEX,
+        fontScale=1,
+        color=(0, 0, 255),
+        thickness=1,
+    )
+    _, img_jpeg = cv2.imencode(".jpeg", img)
+    image_bytes: bytes = img_jpeg.tobytes()
+    return Response(content=image_bytes, media_type="image/jpeg")
 
 @app.get("/working")
 def health():
