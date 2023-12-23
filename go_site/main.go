c3eae9d4fd3a73a4e7bed4a8d819e4d11a1d4d44
florina-alfred <sonofcop@gmail.com>
2023-12-23 04:42:20 +0100

    adding go_site and rust_site

---
+++ go_site/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"math/rand"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/penglongli/gin-metrics/ginmetrics"
+)
+
+type status struct {
+	Working string `json:"working"`
+}
+
+func working(c *gin.Context) {
+
+	ginmetrics.GetMonitor().GetMetric("test_gauge_metric").SetGaugeValue([]string{"label_value1"}, rand.Float64())
+	/* c.JSON(http.StatusOK, gin.H{
+		"working": "ok",
+	}) */
+	var currentStatus = status{Working: "ok"}
+	c.IndentedJSON(http.StatusOK, currentStatus)
+}
+
+func main() {
+
+	router := gin.New()
+	gin.SetMode(gin.ReleaseMode)
+	// gin.DefaultWriter = io.Discard
+	router.LoadHTMLGlob("templates/*")
+
+	metrics := ginmetrics.GetMonitor()
+	metrics.SetMetricPath("/metrics")
+	metrics.SetSlowTime(10)
+	metrics.SetDuration([]float64{0.1, 0.3, 1.2, 5, 10})
+	metrics.Use(router)
+	gaugeMetric := &ginmetrics.Metric{
+		Type:        ginmetrics.Gauge,
+		Name:        "test_gauge_metric",
+		Description: "an example of gauge type metric",
+		Labels:      []string{"label1"},
+	}
+
+	ginmetrics.GetMonitor().AddMetric(gaugeMetric)
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{})
+	})
+
+	router.GET("/working", working)
+
+	router.Run(":8080")
+
+}
