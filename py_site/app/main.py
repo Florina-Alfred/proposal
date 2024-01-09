8d9085994ff235299db8f9db92408e1e7c69a970
florina-alfred <sonofcop@gmail.com>
2024-01-09 19:24:15 +0100

    adding python endpoints

--- py_site/app/main.py
+++ py_site/app/main.py
@@ -8,10 +8,10 @@ import time
 import requests
 
 app = FastAPI(title="Proposal", version="0.5.0")
-TEXT = "Alfred proposes to Florina 3+ times"
 favicon_path = "./app/static/icon.png"
 
 templates = Jinja2Templates(directory="./app/static")
+g = Gauge("test_guage", "description for the test_guage")
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -19,7 +19,38 @@ async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
+@app.get("/api")
+async def api():
+    url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define"
+    querystring = {"term": "set"}
+    headers = {
+        "X-RapidAPI-Key": "e15cce8cedmsh829310d4a331963p1ca8fdjsnd643b41e3b65",
+        "X-RapidAPI-Host": "mashape-community-urban-dictionary.p.rapidapi.com",
+    }
+    return requests.get(url, headers=headers, params=querystring).json()
+
+
+@app.get("/working")
+async def working():
+    g.set(random.random())
+    return {"working": "ok", "language": "python", "time": time.strftime("%X %x %Z")}
+
+
+metrics_app = make_asgi_app()
+app.mount("/metrics", metrics_app)
+
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse(favicon_path)
+
+
+if __name__ == "__main__":
+    uvicorn.run("__main__:app", host="0.0.0.0", port=3000)
+
+
 """ 
+TEXT = "Alfred proposes to Florina 3+ times"
 def gen_frames():
     vid = cv2.VideoCapture(0)
     while True:
@@ -56,36 +87,3 @@ def get_image():
     _, img_jpeg = cv2.imencode(".jpeg", img)
     image_bytes: bytes = img_jpeg.tobytes()
     return Response(content=image_bytes, media_type="image/jpeg") """
-
-
-g = Gauge("test_guage", "description for the test_guage")
-
-
-@app.get("/api")
-async def api():
-    url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define"
-    querystring = {"term": "set"}
-    headers = {
-        "X-RapidAPI-Key": "e15cce8cedmsh829310d4a331963p1ca8fdjsnd643b41e3b65",
-        "X-RapidAPI-Host": "mashape-community-urban-dictionary.p.rapidapi.com",
-    }
-    return requests.get(url, headers=headers, params=querystring).json()
-
-
-@app.get("/working")
-async def working():
-    g.set(random.random())
-    return {"working": "ok", "language": "python", "time": time.strftime("%X %x %Z")}
-
-
-metrics_app = make_asgi_app()
-app.mount("/metrics", metrics_app)
-
-
-@app.get("/favicon.ico", include_in_schema=False)
-async def favicon():
-    return FileResponse(favicon_path)
-
-
-if __name__ == "__main__":
-    uvicorn.run("__main__:app", host="0.0.0.0", port=3000)
